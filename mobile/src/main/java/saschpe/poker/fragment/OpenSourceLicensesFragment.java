eacd2bc7f511895ce98793e13c6f214f7e628867
Sascha Peilicke <sascha@peilicke.de>
2016-12-12 21:18:02 +0100

    Open source licenses

--- mobile/src/main/java/saschpe/poker/fragment/OpenSourceLicensesFragment.java
+++ mobile/src/main/java/saschpe/poker/fragment/OpenSourceLicensesFragment.java
@@ -22,10 +22,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import saschpe.poker.R;
 
 public class OpenSourceLicensesFragment extends Fragment {
+    private static final String LICENSES_HTML_PATH = "file:///android_asset/licenses.html";
+
+    private WebView webView;
+
     /**
      * Called to do initial creation of a fragment.  This is called after
      * {@link #onAttach(Activity)} and before
@@ -69,8 +78,9 @@ public class OpenSourceLicensesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.view_help_card, container, false);
-        return rootView;
+        View view = inflater.inflate(R.layout.fragment_open_source_licenses, container, false);
+        webView = (WebView) view.findViewById(R.id.web_view);
+        return view;
     }
 
     /**
@@ -86,5 +96,10 @@ public class OpenSourceLicensesFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.loadUrl(LICENSES_HTML_PATH);
+        webView.setScrollContainer(false);
     }
 }
